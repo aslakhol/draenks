b423944cb1ec52e2c5b3407fd22ba24a9fea2081
Aslak Hollund <aslakhol@gmail.com>
2022-03-29 22:17:26 +0200

    add trpc

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,14 @@
+import { trpc } from "@/utils/trpc";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <div className="text-2xl text-red-400">Draenks</div>;
+  const { data, isLoading } = trpc.useQuery(["hello", { text: "me" }]);
+
+  if (isLoading) {
+    return <div>Loading... </div>;
+  }
+
+  return <div className="text-2xl text-red-400">{data?.greeting}</div>;
 };
 
 export default Home;
