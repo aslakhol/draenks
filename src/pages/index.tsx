fdddd9646f1b4cc0eda0bff9b12f0482aa8c72f4
Aslak Hollund <aslakhol@gmail.com>
2022-04-02 19:34:03 +0200

    add prisma and planetscale

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,40 @@
 import { trpc } from "@/utils/trpc";
 import type { NextPage } from "next";
+import { useEffect, useState } from "react";
 
 const Home: NextPage = () => {
+  const [input, setInput] = useState("");
   const { data, isLoading } = trpc.useQuery(["hello", { text: "me" }]);
+
+  const { data: foob } = trpc.useQuery(["world"]);
+
+  const { data: drinks } = trpc.useQuery(["drinks"]);
+
+  const createDrinkMutation = trpc.useMutation("create-drink");
+
+  useEffect(() => {
+    console.log(drinks);
+  }, [drinks]);
+
+  const handleClick = () => {
+    createDrinkMutation.mutate({ name: input });
+  };
 
   if (isLoading) {
     return <div>Loading... </div>;
   }
 
-  return <div className="text-2xl text-red-400">{data?.greeting}</div>;
+  return (
+    <div className="text-2xl text-red-400">
+      <p>
+        {data?.greeting} {foob}
+      </p>
+      <input value={input} onChange={(e) => setInput(e.target.value)} />
+      <button onClick={() => handleClick()} className="bg-green-200">
+        Press
+      </button>
+    </div>
+  );
 };
 
 export default Home;
