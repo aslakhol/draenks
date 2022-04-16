d88036526a401116793b9d128d3cfb70af41ebcc
Aslak Hollund <aslakhol@gmail.com>
2022-04-16 21:52:37 +0200

    style button

--- src/pages/ingredients/new.tsx
+++ src/pages/ingredients/new.tsx
@@ -44,7 +44,12 @@ const NewIngredientPage: NextPage = () => {
           register={register}
           fieldError={errors.description}
         />
-        <button type="submit">submit</button>
+        <button
+          type="submit"
+          className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        >
+          Save
+        </button>
       </form>
 
       {data?.map((ingredient) => (
