3e0a56abfc747a267cc11acd5560e496b2be080f
Aslak Hollund <aslakhol@gmail.com>
2022-05-19 23:01:16 +0200

    remove some more stuff from router

--- src/backend/router/index.ts
+++ src/backend/router/index.ts
@@ -3,27 +3,9 @@ import { z } from "zod";
 
 import { prisma } from "@/backend/utils/prisma";
 import { newDrinkFormSchema } from "@/features/drinks/formValidation";
-import { create } from "domain";
 
 export const appRouter = trpc
   .router()
-  .query("hello", {
-    input: z
-      .object({
-        text: z.string().nullish(),
-      })
-      .nullish(),
-    resolve({ input }) {
-      return {
-        greeting: `hello ${input?.text ?? "world"}`,
-      };
-    },
-  })
-  .query("world", {
-    resolve() {
-      return "woooooorld";
-    },
-  })
   .query("drinks", {
     resolve: () => {
       return prisma.drinks.findMany({ include: { ingredients: true } });
@@ -64,9 +46,3 @@ export const appRouter = trpc
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
-
-const bar = () => {};
-
-function foo() {
-  console.log();
-}
